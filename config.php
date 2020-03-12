56a67be4941008e61549f2c2c76e13249fc10091
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2020-03-13 08:25:35 +1000

    fixed config

--- config.php
+++ config.php
@@ -4,11 +4,12 @@ function getAllWGUIAuthConfig() {
 
   $wguiauthconfig = array();
   $wguiauthconfig['wguiauth.application.language'] = 'en-US';
-  // $wguiauthconfig['wguiauth.db'] = 'sqlite:/var/www/html/db/wgui-auth.db';
+  $wguiauthconfig['wguiauth.db'] = 'sqlite:/var/www/db/wgui-auth.db';
   // $wguiauthconfig['wguiauth.db'] = 'pgsql:host=pgsql';
   // $wguiauthconfig['wguiauth.db.user'] = 'postgres';
-  $wguiauthconfig['wguiauth.db.password'] = 'password';
-  $wguiauthconfig['wguiauth.logger.path'] = 'stdout';
+  // $wguiauthconfig['wguiauth.db.password'] = 'password';
+  $wguiauthconfig['wguiauth.logger.path'] = 'logs/app.log';
+  // $wguiauthconfig['wguiauth.logger.path'] = 'stdout';
   $wguiauthconfig['wguiauth.logger.level'] = \Monolog\Logger::WARNING;
   $wguiauthconfig['wguiauth.twig.template.path'] = 'templates';
   $wguiauthconfig['wguiauth.wgui.url'] = '/wireguardui/';
